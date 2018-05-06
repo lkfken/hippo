70d9abdd2d478858949898f4ca7328bde0549441
Kenneth Leung <kenneth@leungs.us>
2018-05-06 16:57:38 -0700

    correct N3 to handle address 1 to 3.

--- lib/hippo/segments/N3.rb
+++ lib/hippo/segments/N3.rb
@@ -3,7 +3,7 @@ module Hippo::Segments
 
     segment_identifier 'N3'
 
-    field :name                 => 'AddressInformation',
+    field :name                 => 'AddressLine1',
           :sequence             => 1,
           :datatype             => :string,
           :minimum              => 1,
@@ -11,13 +11,19 @@ module Hippo::Segments
           :required             => true,
           :data_element_number  => 166
 
-    field :name                 => 'AddressInformation',
+    field :name                 => 'AddressLine2',
           :sequence             => 2,
           :datatype             => :string,
           :minimum              => 1,
           :maximum              => 55,
           :required             => false,
           :data_element_number  => 166
-
+    field :name                 => 'AddressLine3',
+          :sequence             => 3,
+          :datatype             => :string,
+          :minimum              => 1,
+          :maximum              => 55,
+          :required             => false,
+          :data_element_number  => 166
   end
 end
